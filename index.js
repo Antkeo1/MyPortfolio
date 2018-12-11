b8d0556f3a0557fceef20e85286a966e2a81f380
Antkeo1 <sleav143@gmail.com>
2018-12-11 08:26:03 -0500

    fixed logic of page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   console.log('ready')
-});
+})
 // get aboutMeContent, then hide it
 $('#aboutMeContent').hide()
 $('#contactContent').hide()
@@ -8,48 +8,46 @@ $('#resumeContent').hide()
 $('#projectContent').hide()
 
 // get home id, when clicked
-$("#home").click(function() {
+$('#home').click(function () {
   // get aboutMeContent and hide it
-  $('#aboutMeContent').hide();
+  $('#aboutMeContent').hide()
   // get homeContent and show it
-  $('#homeContent').show();
+  $('#homeContent').show()
   // get contactContent and hide it
-  $('#contactContent').hide();
-  $('#resumeContent').hide();
+  $('#contactContent').hide()
+  $('#resumeContent').hide()
   $('#projectContent').hide()
-});
+})
 
 // get about id when clicked
-$("#about").click(function() {
-  $('#aboutMeContent').show();
-  $('#homeContent').hide();
-  $('#contactContent').hide();
-  $('#resumeContent').hide();
+$('#about').click(function () {
+  $('#aboutMeContent').show()
+  $('#homeContent').hide()
+  $('#contactContent').hide()
+  $('#resumeContent').hide()
   $('#projectContent').hide()
+})
 
-});
-
-$("#project").click(function() {
-  $('#aboutMeContent').hide();
-  $('#homeContent').hide();
-  $('#contactContent').hide();
-  $('#resumeContent').hide();
+$('#project').click(function () {
+  ('#projectContent').hide()
+  $('#homeContent').hide()
+  $('#contactContent').hide()
+  $('#resumeContent').hide()
   $('#projectContent').show()
-});
+})
 
-$("#resume").click(function() {
-  $('#aboutMeContent').hide();
-  $('#homeContent').hide();
-  $('#contactContent').hide();
-  $('#resumeContent').show();
+$('#resume').click(function () {
+  $('#aboutMeContent').hide()
+  $('#homeContent').hide()
+  $('#contactContent').hide()
+  $('#resumeContent').show()
   $('#projectContent').hide()
-});
+})
 
-
-$("#contact").click(function() {
-  $('#contactContent').show();
-  $('#homeContent').hide();
-  $('#aboutMeContent').hide();
-  $('#resumeContent').hide();
+$('#contact').click(function () {
+  $('#contactContent').show()
+  $('#homeContent').hide()
+  $('#aboutMeContent').hide()
+  $('#resumeContent').hide()
   $('#projectContent').hide()
-});
+})
